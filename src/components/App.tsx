01b09deef7721d0cc384190935f9d46d9da5ccf6
none Frontier <liuyiquancov5@gmail.com>
2020-05-25 08:33:59 +0800

    update App

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -9,6 +9,21 @@ import './App.css'
 
 const { Header, Content, Footer} = Layout
 
-const App = ({match}: any) => {}
+const App = ({match}: any) => {
+    let defaultKey = match.url.replace('/','') || 'employee'
+    return <ConfigProvider locale={zh_CN}>
+            <Layout>
+                <Header>
+                    <Menu
+                        theme='dark'
+                        mode='horizontal'
+                        defaultOpenKeys={[defaultKey]}
+                        className='menu'
+                    >
+                    </Menu>
+                </Header>
+            </Layout>
+           </ConfigProvider>
+}
 
 export default App;
